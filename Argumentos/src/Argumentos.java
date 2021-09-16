ce0b94fdea31019ddbba2ad2b3b55067dbfa28dd
Pedro Nunes Moura <61888421+Pedro-nunes07@users.noreply.github.com>
2021-09-16 15:20:58 -0300

    Add files via upload
    Aula do treinamento magna - https://loiane.training/continuar-curso/java-basico

---
+++ Argumentos/src/Argumentos.java
@@ -0,0 +1,10 @@
+public class Argumentos {
+    public static void main(String[] args) {
+        System.out.println("Voce digitou " + args[0]);
+    }
+}
+/**
+ Se você só inserir uma frase, segundo a String só irá exibir a primeira palavra (da posição 0)
+
+ Se você inserir uma frase ou mais de uma palavra, para exibição será necessaria a inserção de aspas duplas
+ */
