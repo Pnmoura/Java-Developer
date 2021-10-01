d9d1e8855e53648ef742352c0851faccbd7f438c
Pedro Nunes Moura <61888421+Pnmoura@users.noreply.github.com>
2021-10-01 16:37:19 -0300

    Add files via upload

---
+++ Aula71/Teste.java
@@ -0,0 +1,13 @@
+package Aula71;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		int[] array = {1, 2, 3, 4, 5}; //valor que esta sendo somado
+		
+		MinhaThreadSoma t1 = new MinhaThreadSoma("#1", array);
+		MinhaThreadSoma t2 = new MinhaThreadSoma("#2", array);
+	}
+
+}
